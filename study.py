bc150cf40901bac1ac3cb85022bc4e43bd86c548
HEHM26 <hagi1126@naver.com>
2019-10-22 13:04:14 +0900

    2019.10.22 Web Crawling_Hit Game Chart

---
+++ study.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+#크롬 드라이버 주소 C:\Users\Mark\Desktop\GitHub\driver
+driver = webdriver.Chrome("C:/Users/Mark/Desktop/GitHub/driver/chromedriver.exe")
+driver.get("http://www.gevolution.co.kr/rank/ios")
+soup = BeautifulSoup(driver.page_source, "lxml")
+
+i = 1
+rawdata = []
+for data in soup.findAll(name="div", attrs={"class":"grp"}):
+    stripdata = data.text.strip()
+    rawdata = stripdata.split('\n')
+
+    print(str(i)+'위  : '+rawdata[0])
+    print(rawdata[1]+'\n')
+
+
+    #print(str(i)+"위 : "+data.text.strip())
+    i += 1
+    if i == 5:
+        break
